94b6315f445bd64cb4eb354c047772dff1a9112f
ricardog <ricardog@localhost>
2016-02-12 13:47:07 -0500

    second initial commit

---
+++ GuessGameRHG/src/edu/saintjoe/cs/ricardog/guessgamerhg/GameLauncher.java
@@ -0,0 +1,9 @@
+package edu.saintjoe.cs.ricardog.guessgamerhg;
+
+public class GameLauncher {
+	public static void main (String[] args) {
+		GuessGame game = new GuessGame();
+		game.startGame();
+	}
+
+}
--- GuessGameRHG/src/edu/saintjoe/cs/ricardog/guessgamerhg/Player.java
+++ GuessGameRHG/src/edu/saintjoe/cs/ricardog/guessgamerhg/Player.java
@@ -1,5 +1,11 @@
 package edu.saintjoe.cs.ricardog.guessgamerhg;
 
 public class Player {
+	int number = 0;  // where the guess goes
+	
+	public void guess() {
+		number = (int) (Math.random() * 10);
+		System.out.println("I'm guessing " + number);
+	}
 
 }
